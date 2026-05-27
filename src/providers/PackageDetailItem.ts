cb6a61e5f66956c2723032fc75ab64036a7ea381
Alex Green <deadpoolweid@yandex.ru>
2026-05-27 16:44:08 +0700

    Fix detail row context value

--- src/providers/PackageDetailItem.ts
+++ src/providers/PackageDetailItem.ts
@@ -3,6 +3,6 @@ import * as vscode from 'vscode';
 export class PackageDetailItem extends vscode.TreeItem {
   constructor(label: string) {
     super(label, vscode.TreeItemCollapsibleState.None);
-    this.contextValue = 'packageDetail';
+    this.contextValue = 'detail';
   }
 }
